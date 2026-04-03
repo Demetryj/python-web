5fd39f92a4716ea93587afd672a7fe095b09bc4d
Demetryj <sdm17@ukr.net>
2026-04-03 21:59:56 +0300

    add an option that the query execution function accepts parameters for sql

--- pw-6/select_data.py
+++ pw-6/select_data.py
@@ -4,16 +4,19 @@ from connection import create_connection
 
 Row = tuple[object, ...]
 
-
-def execute_select_query(sql: str) -> list[Row]:
+            
+def execute_select_query(sql: str, params: tuple | list | dict | None = None) -> list[Row]:
     """Execute a SELECT query and return all fetched rows."""
+    
     with create_connection() as connection:
         try:
             with connection.cursor() as cursor:
-                cursor.execute(sql)
+                if params is None:
+                    cursor.execute(sql)
+                else:
+                    cursor.execute(sql, params)
                 return cursor.fetchall()
         except Error as err:
             connection.rollback()
             print(err)
             raise
-            
