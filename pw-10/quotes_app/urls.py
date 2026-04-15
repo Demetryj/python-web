cb94c6e5733394b658959578d3cda0c0dcc55db2
Demetryj <sdm17@ukr.net>
2026-04-15 23:40:35 +0300

    add author addition and quote addition with the ability to reverse author and tags (optional)

--- pw-10/quotes_app/urls.py
+++ pw-10/quotes_app/urls.py
@@ -5,5 +5,7 @@ app_name = "quotes_app"
 
 urlpatterns = [
     path("", views.main, name="main"),
-    path("author/<int:author_id>", views.about_author, name="author"),
+    path("author/<int:author_id>/", views.about_author, name="author"),
+    path("add-author/", views.add_author, name="add_author"),
+    path("add-quote/", views.add_quote, name="add_quote"),
 ]
--- pw-10/quotes_app/views.py
+++ pw-10/quotes_app/views.py
@@ -1,13 +1,15 @@
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Quote, Author
+from .forms import AuthorForm, QuoteForm
 
 PER_PAGE = 10
 
 
 # Create your views here.
 def main(request):
+    """Render the main page with paginated quotes."""
     quote_list = Quote.objects.all().order_by("id")
     paginator = Paginator(quote_list, PER_PAGE)  # Show PER_PAGE contacts per page.
 
@@ -17,5 +19,32 @@ def main(request):
 
 
 def about_author(request, author_id):
+    """Render details page for a single author."""
     author_obj = get_object_or_404(Author, id=author_id)
     return render(request, "quotes_app/author.html", {"author_obj": author_obj})
+
+
+def add_author(request):
+    """Handle creation of a new author via AuthorForm."""
+    if request.method == "POST":
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes_app:main')
+        else:
+            return render(request, 'noteapp/add_author.html', {'form': form})
+        
+    return render(request, "quotes_app/add_author.html", {'form': AuthorForm()})
+
+
+def add_quote(request):
+    """Handle creation of a new quote with author and tags."""
+    if request.method == "POST":
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("quotes_app:main")
+    else:
+        form = QuoteForm()
+
+    return render(request, "quotes_app/add-quote.html", {"form": form})
